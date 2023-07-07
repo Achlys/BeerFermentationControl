e03ee3c19c2b582aa300df1306454026f09ad4e7
SERRGORA <SERRGORA@W10-BOIK-PRO-24.sare.gipuzkoa.net>
2023-07-07 14:08:53 +0200

    Commit inicial.

---
+++ BeerFermentationControlWEB/src/test/java/org/xer/BeerFermentationControlWEB/BeerFermentationControlWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xer.BeerFermentationControlWEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerFermentationControlWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
