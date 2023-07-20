c7ae0159aeb481fdf50f0511bd71406ebbe5a76f
SERRGORA <SERRGORA@W10-BOIK-PRO-26.sare.gipuzkoa.net>
2023-07-20 14:21:57 +0200

    - Remove Jakarta EE. - Make a little example to see if it works.

---
+++ BeerFermControlWEB/src/main/java/org/xer/beerfermcontrol/web/InitController.java
@@ -0,0 +1,19 @@
+package org.xer.beerfermcontrol.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * 
+ * @author Achlys
+ */
+@Controller
+public class InitController {
+    
+    @RequestMapping("/prueba")
+    public String prueba(){
+        return "prueba";
+    }
+    
+}
