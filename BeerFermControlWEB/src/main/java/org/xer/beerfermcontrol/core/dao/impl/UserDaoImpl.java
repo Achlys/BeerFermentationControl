00dc14688d9126848221300bb1b62004115f05ad
Achlys <???>
2023-08-02 15:48:32 +0200

    - Validation and DB connection working.

--- BeerFermControlWEB/src/main/java/org/xer/beerfermcontrol/core/dao/impl/UserDaoImpl.java
+++ BeerFermControlWEB/src/main/java/org/xer/beerfermcontrol/core/dao/impl/UserDaoImpl.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -16,25 +17,30 @@ import org.xer.beerfermcontrol.core.dao.UserDao;
  */
 @Repository
 public class UserDaoImpl implements UserDao {
-    
+
     @Autowired
     private NamedParameterJdbcTemplate jdbc;
 
     @Override
     public User getUsuario(String userName) {
-        String sql = "SELECT * FROM USER WHERE USERNAME = :username";
-        return jdbc.queryForObject(sql, Collections.singletonMap("username", userName), new RowMapper<User>(){
-            @Override
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                User user = new User();
-                user.setId(rs.getInt("ID"));
-                user.setUsername(rs.getString("USERNAME").trim());
-                user.setPassword(rs.getString("PASSWORD").trim());
-                user.setStartDate(rs.getDate("START_DATE"));
-                user.setEndDate(rs.getDate("END_DATE"));
-                return user;
-            }
-        });
+        String sql = "SELECT * FROM USERS WHERE USERNAME = :username";
+        try {
+            return jdbc.queryForObject(sql, Collections.singletonMap("username", userName), new RowMapper<User>() {
+                @Override
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    User user = new User();
+                    user.setId(rs.getInt("ID"));
+                    user.setUsername(rs.getString("USERNAME").trim());
+                    user.setPassword(rs.getString("PASSWORD").trim());
+                    user.setStartDate(rs.getDate("START_DATE"));
+                    user.setEndDate(rs.getDate("END_DATE"));
+                    return user;
+                }
+            });
+        } catch (EmptyResultDataAccessException e) {
+            // The username doesn't exist
+            return null;
+        }
     }
-    
+
 }
--- BeerFermControlWEB/src/main/java/org/xer/beerfermcontrol/web/controller/InitController.java
+++ BeerFermControlWEB/src/main/java/org/xer/beerfermcontrol/web/controller/InitController.java
@@ -1,12 +1,12 @@
 package org.xer.beerfermcontrol.web.controller;
 
+import javax.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,37 +16,38 @@ import org.xer.beerfermcontrol.core.facade.BeerFermControlFacade;
 import org.xer.beerfermcontrol.web.util.WebConstants;
 
 /**
- * 
- * 
+ *
+ *
  * @author Achlys
  */
 @Controller
 @SessionAttributes({WebConstants.USER})
 public class InitController {
-    
+
     private static final Logger LOGGER = LogManager.getLogger(InitController.class);
-    
+
     @Autowired
     private BeerFermControlFacade beerFermControlFacade;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loadLogin(Model model){
-        if(!model.asMap().containsKey(WebConstants.USER)){
+    public String loadLogin(Model model) {
+        if (!model.asMap().containsKey(WebConstants.USER)) {
             model.addAttribute(WebConstants.USER, new User());
         }
         return "login";
     }
-    
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@ModelAttribute(WebConstants.USER) @Validated User logUser, BindingResult bindingResult, Model model){
-        if(bindingResult.hasErrors()){
+    public String login(@Valid @ModelAttribute(WebConstants.USER) User logUser, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
             return "login";
         }
         User user = beerFermControlFacade.getUser(logUser.getUsername(), logUser.getPassword());
         if (user == null) {
             bindingResult.rejectValue("username", "error.login");
+            bindingResult.rejectValue("password", "error.vacio");
             return "login";
-        }else{
+        } else {
             return "redirect:/";
         }
     }
