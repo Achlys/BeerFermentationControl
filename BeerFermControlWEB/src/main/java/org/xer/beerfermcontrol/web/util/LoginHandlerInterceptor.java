3a8a5de28e8a2d49ef5299b6a818d4b4db12e8c1
Achlys <???>
2023-08-02 20:29:54 +0200

    - Added login security.

---
+++ BeerFermControlWEB/src/main/java/org/xer/beerfermcontrol/web/util/LoginHandlerInterceptor.java
@@ -0,0 +1,30 @@
+package org.xer.beerfermcontrol.web.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.xer.beerfermcontrol.core.bean.User;
+
+/**
+ *
+ * @author Achlys
+ */
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+    
+    private static final Logger LOGGER = LogManager.getLogger(LoginHandlerInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User userSession = (User) request.getSession().getAttribute(WebConstants.USER);
+        if (!request.getServletPath().startsWith("/login") && userSession == null) {
+            LOGGER.error("Ha intentado acceder al path: " + request.getRequestURI() + ", sin estar logeado!!!");
+            response.sendRedirect("/login");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
